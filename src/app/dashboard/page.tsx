edcfe959ebf6a9725f27de6ffbcece70c7240bcd
John Barnes <ohenesetwumasi@gmail.com>
2023-10-08 23:36:11 +0000

    fix: typescript type for status

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -56,7 +56,7 @@ const transactions = [
   },
   // More transactions...
 ];
-const statusStyles = {
+const statusStyles: Record<string, string> = {
   success: "bg-green-100 text-green-800",
   processing: "bg-yellow-100 text-yellow-800",
   failed: "bg-gray-100 text-gray-800",
